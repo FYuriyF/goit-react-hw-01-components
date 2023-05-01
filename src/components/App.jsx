a7cbcf382919460c0ae29101bf938413948663e9
FYuriyF <jufedor@gmail.com>
2023-05-01 20:55:09 +0300

    first task styling through emotion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
 import { Profile } from './Profile/Profile';
 import user from 'data/user.json';
+import { Container, SectionWrapper } from './App.styled';
+import { Global } from '@emotion/react';
+import { globalStyles } from 'constants/globalStyles';
 
 export const App = () => {
   return (
-    <div>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-    </div>
+    <Container>
+      <Global styles={globalStyles} />
+      <SectionWrapper>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </SectionWrapper>
+    </Container>
   );
 };
+
+//  <Profile
+//    username={user.username}
+//    tag={user.tag}
+//    location={user.location}
+//    avatar={user.avatar}
+//    stats={user.stats}
+//  />;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,38 +1,82 @@
 import PropTypes from 'prop-types';
-import css from '../Profile/Profile.module.css';
+import {
+  ProfileCard,
+  Description,
+  Avatar,
+  Name,
+  Text,
+  Stats,
+  Item,
+  Label,
+  Value,
+} from './Profile.styled';
+import { formatNumber } from '../../utils/fotmatNumber';
+// import css from '../Profile/Profile.module.css';
 
-export const Profile = ({ username, tag, location, avatar, stats }) => {
+export const Profile = ({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+}) => {
   return (
-    <div className="profile">
-      <div className={css.description}>
-        <img src={avatar} alt="User avatar" className={css.avatar} />
-        <p className={css.name}>{username}</p>
-        <p className={css.tag}>@{tag}</p>
-        <p className={css.location}>{location}</p>
-      </div>
+    <ProfileCard>
+      <Description>
+        <Avatar src={avatar} alt={username} />
+        <Name>{username}</Name>
+        <Text>{`@${tag}`}</Text>
+        <Text>{location}</Text>
+      </Description>
 
-      <ul className={css.stats}>
-        <li className={css.statsItem}>
-          <span className={css.label}>Followers</span>
-          <span className={css.quantity}>{stats.followers}</span>
-        </li>
-        <li className={css.statsItem}>
-          <span className={css.label}>Views</span>
-          <span className={css.quantity}>{stats.views}</span>
-        </li>
-        <li className={css.statsItem}>
-          <span className={css.label}>Likes</span>
-          <span className={css.quantity}>{stats.likes}</span>
-        </li>
-      </ul>
-    </div>
+      <Stats>
+        <Item>
+          <Label>Followers</Label>
+          {/* <Value>{new Intl.NumberFormat('en-US').format(followers)}</Value> */}
+          <Value>{formatNumber(followers)}</Value>
+        </Item>
+        <Item>
+          <Label>Views</Label>
+          <Value>{formatNumber(views)}</Value>
+        </Item>
+        <Item>
+          <Label>Likes</Label>
+          <Value>{formatNumber(likes)}</Value>
+        </Item>
+      </Stats>
+    </ProfileCard>
   );
+  //   <div className="profile">
+  //     <div className={css.description}>
+  //       <img src={avatar} alt="User avatar" className={css.avatar} />
+  //       <p className={css.name}>{username}</p>
+  //       <p className={css.tag}>@{tag}</p>
+  //       <p className={css.location}>{location}</p>
+  //     </div>
+
+  //     <ul className={css.stats}>
+  //       <li className={css.statsItem}>
+  //         <span className={css.label}>Followers</span>
+  //         <span className={css.quantity}>{followers}</span>
+  //       </li>
+  //       <li className={css.statsItem}>
+  //         <span className={css.label}>Views</span>
+  //         <span className={css.quantity}>{views}</span>
+  //       </li>
+  //       <li className={css.statsItem}>
+  //         <span className={css.label}>Likes</span>
+  //         <span className={css.quantity}>{likes}</span>
+  //       </li>
+  //     </ul>
+  //   </div>
+  // );
 };
 
 Profile.propTypes = {
   username: PropTypes.string.isRequired,
-  tag: PropTypes.string.isRequired,
+  tag: PropTypes.PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
   stats: PropTypes.shape({
     followers: PropTypes.number.isRequired,
     views: PropTypes.number.isRequired,
